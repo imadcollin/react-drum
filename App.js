9a155170194e1469ee16b41662b1be0695c9ee67
imadcollin <imad.collin@gmail.com>
2021-08-03 21:10:40 +0200

    Call list of button for samples

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {SafeAreaView, Text, View, Alert} from 'react-native';
+import {SafeAreaView, Text, View, Alert, FlatList} from 'react-native';
 import Header from './src/componants/Headers/Header';
 import AppStyles from './AppStyle';
 import Layout from './src/componants/Layout/Layout';
 import PressButton from './src/componants/Button/PressButton';
+import samples from './src/constants/data';
 const App = () => {
+  const renderItem = ({item}) => (<PressButton title={item.title}></PressButton>);
+
   return (
     <SafeAreaView style={AppStyles.container}>
       <Header></Header>
       <Layout></Layout>
-      <PressButton title="btn 1"></PressButton>
-      <PressButton title="btn 2"></PressButton>
+      <FlatList
+        data={samples}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}></FlatList>
       <Text>App Container</Text>
       <View style={AppStyles.view}>
         <Text>App View</Text>
--- src/componants/Button/PressButton.js
+++ src/componants/Button/PressButton.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
+import {sample} from 'lodash';
 var Sound = require('react-native-sound');
-
+import Player from '../../helper/Player';
 const PressButton = props => {
-  const play = () => {
-    Sound.setCategory('Playback');
-    var whoosh = new Sound('my.mp3', Sound.MAIN_BUNDLE, error => {
-      if (error) {
-        console.log('failed to load the sound', error);
-        return;
-      }
-      whoosh.play(success => {
-        if (success) {
-          console.log('successfully finished playing');
-        } else {
-          console.log('playback failed due to audio decoding errors');
-        }
-      });
-    });
-  };
   return (
     <View>
       <Button
         title={props.title}
-        onPress={() => console.log(`${props.title} is Pressed!`)}></Button>
-      <Button title="play" color="#555" onPress={play}></Button>
+        color="#555"
+        onPress={() => new Player(props.title).play(props.title)}></Button>
+      <Text> PLay</Text>
+      <Button
+        title="Pause"
+        onPress={() => new Player(props.title).pause(props.title)}></Button>
     </View>
   );
 };
