e473fe2624b60834ef0e1dab29914dd4541a9eb0
imadcollin <imad.collin@gmail.com>
2021-08-04 23:27:33 +0200

    Make play pause functionality

--- src/componants/Button/PressButton.js
+++ src/componants/Button/PressButton.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React ,{ useState }  from 'react';
 import {Text, View, Button} from 'react-native';
-import {sample} from 'lodash';
 var Sound = require('react-native-sound');
 import Player from '../../helper/Player';
 const PressButton = props => {
+   const [player] = useState(new Player(props.title))
+  
   return (
     <View>
       <Button
         title={props.title}
         color="#555"
-        onPress={() => new Player(props.title).play(props.title)}></Button>
+        onPress={()=> player.play(props.title)
+        }></Button>
       <Text> PLay</Text>
       <Button
         title="Pause"
-        onPress={() => new Player(props.title).pause(props.title)}></Button>
+        onPress={()=>   player.pause}></Button>
     </View>
   );
 };
--- src/helper/Player.js
+++ src/helper/Player.js
@@ -2,26 +2,17 @@ var Sound = require('react-native-sound');
 
 class Player {
   constructor(title) {
-    console.log('reac');
     this.title = title;
     Sound.setCategory('Playback');
   }
-  play = title => {
-    var test = new Sound(`${title}.mp3`, Sound.MAIN_BUNDLE, error => {
-      if (error) {
-        console.log('failed to load the sound', error);
-        return;
-      }
-      test.play(success => {
-        if (success) {
-          console.log('successfully finished playing');
-        } else {
-          console.log('playback failed due to audio decoding errors');
-        }
-      });
-    });
+
+  play(title) {
+    var that = this;
+    this.sound = new Sound(`${title}.mp3`, Sound.MAIN_BUNDLE, () => that.sound.play());
   };
-  pause = x => console.log('rrr');
+  pause() {
+    this.sound.pause();
+  }
 }
 
 export default Player;
