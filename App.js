1e1af1ed5361aac20184c351c2c237ecb4b43a67
imadcollin <imad.collin@gmail.com>
2021-08-05 14:07:45 +0200

    Make stop stand alone componant

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React ,{ useState }  from 'react';
 import {SafeAreaView, Text, View, Alert, FlatList} from 'react-native';
 import Header from './src/componants/Headers/Header';
 import AppStyles from './AppStyle';
 import Layout from './src/componants/Layout/Layout';
 import PressButton from './src/componants/Button/PressButton';
 import samples from './src/constants/data';
+import StopButton from './src/componants/Button/StopButton';
+import Player from './src/helper/Player';
 const App = () => {
-  const renderItem = ({item}) => (<PressButton title={item.title}></PressButton>);
+  const [player] = useState(new Player());
+
+  const renderItem = ({item}) => (<PressButton title={item.title} player={player}></PressButton>);
 
   return (
     <SafeAreaView style={AppStyles.container}>
@@ -18,6 +22,7 @@ const App = () => {
         keyExtractor={item => item.id}></FlatList>
       <Text>App Container</Text>
       <View style={AppStyles.view}>
+        <StopButton  player={player}></StopButton>
         <Text>App View</Text>
       </View>
     </SafeAreaView>
--- src/componants/Button/PressButton.js
+++ src/componants/Button/PressButton.js
@@ -1,21 +1,13 @@
-import React ,{ useState }  from 'react';
-import {Text, View, Button} from 'react-native';
-var Sound = require('react-native-sound');
-import Player from '../../helper/Player';
+import React  from 'react';
+import {View, Button} from 'react-native';
 const PressButton = props => {
-   const [player] = useState(new Player(props.title))
-  
   return (
     <View>
       <Button
         title={props.title}
         color="#555"
-        onPress={()=> player.play(props.title)
+        onPress={()=> props.player.play(props.title)
         }></Button>
-      <Text> PLay</Text>
-      <Button
-        title="Pause"
-        onPress={()=>   player.pause}></Button>
     </View>
   );
 };
