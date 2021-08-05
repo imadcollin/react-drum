410a2476726d1e0f191312a8a4b432c6c90c7774
imadcollin <imad.collin@gmail.com>
2021-08-05 22:13:08 +0200

    Fix player not releasing resources

--- src/helper/Player.js
+++ src/helper/Player.js
@@ -4,26 +4,34 @@ class Player {
   constructor(title) {
     this.title = title;
     Sound.setCategory('Playback');
+    this.sound = new Sound(`${title}.mp3`)//Default Instance. 
   }
 
   play(title) {
+    this.sound.release();
     this.sound = new Sound(`${title}.mp3`, Sound.MAIN_BUNDLE, (error) => {
       if (error) {
         console.log('something went wrong');
         return;
       }
+      console.log("load..", this.sound.isLoaded());
       this.sound.play((success) => {
+        console.log("playing..", this.sound.isPlaying());
         if (success) {
           console.log("played succesfully")
         } else {
           console.log("failed to play...!")
         };
-      })
-    })
+      });
+    });
+
   };
   stop() {
-    this.sound.stop();
-    this.sound.release();
+    if (this.sound.isPlaying()) {
+      //this.sound.reset();
+      this.sound.stop();
+      this.sound.release();
+    }
   }
 }
 
