295f0349ccb19741c9813a848fac035ef756dfbe
imadcollin <imad.collin@gmail.com>
2021-08-06 23:57:16 +0200

    Add some style for buttons

--- src/componants/Button/PressButton.js
+++ src/componants/Button/PressButton.js
@@ -1,14 +1,40 @@
 import React  from 'react';
-import {View, Button} from 'react-native';
+import {View, Button,StyleSheet} from 'react-native';
 const PressButton = props => {
   return (
-    <View>
-      <Button
+    <View style={PressStyle}>
+      <Button style={PressStyle.button}
         title={props.title}
-        color="#555"
         onPress={()=> props.player.play(props.title)
         }></Button>
     </View>
   );
 };
 export default PressButton;
+
+const PressStyle=StyleSheet.create(
+  {
+    container:{
+      display:'flex',
+      flexDirection:'row',
+      backgroundColor:'yellow'
+      
+    },
+      button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+      },
+      text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+      }
+  }
+)
