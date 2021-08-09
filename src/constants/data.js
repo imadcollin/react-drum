d19b1ebf8d50b89511be9529c110956dfae0b913
imadcollin <imad.collin@gmail.com>
2021-08-09 23:58:53 +0200

    Add more sample data

--- src/constants/data.js
+++ src/constants/data.js
@@ -14,5 +14,45 @@ const samples = [
     title: 'sample3',
     text: 'this it sample 3'
   },
+  {
+    id: 4,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 5,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 6,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 7,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 8,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 9,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 10,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
+  {
+    id: 11,
+    title: 'sample2',
+    text: 'this is sample2 2'
+  },
 ];
 export default samples;
