64e739b500dcd5f29db2fe823c9decdf79bfd1f9
imadcollin <imad.collin@gmail.com>
2021-08-10 23:29:02 +0200

    Clean unused button style

--- src/componants/Button/PressButton.js
+++ src/componants/Button/PressButton.js
@@ -1,40 +1,10 @@
-import React  from 'react';
-import {View, Button,StyleSheet} from 'react-native';
+import React from 'react';
+import {Button} from 'react-native';
 const PressButton = props => {
   return (
-    <View style={PressStyle}>
-      <Button style={PressStyle.button}
-        title={props.title}
-        onPress={()=> props.player.play(props.title)
-        }></Button>
-    </View>
+    <Button
+      title={props.title}
+      onPress={() => props.player.play(props.title)}></Button>
   );
 };
 export default PressButton;
-
-const PressStyle=StyleSheet.create(
-  {
-    container:{
-      display:'flex',
-      flexDirection:'row',
-      backgroundColor:'yellow'
-      
-    },
-      button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'black',
-      },
-      text: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-      }
-  }
-)
