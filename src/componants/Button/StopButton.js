744b7a99f2067324ead04cb48904ebd7a33a70e5
imadcollin <imad.collin@gmail.com>
2021-08-11 23:05:45 +0200

    Fix button style

--- src/componants/Button/StopButton.js
+++ src/componants/Button/StopButton.js
@@ -1,12 +1,15 @@
-import React ,{ useState }  from 'react';
-import {Text, View, Button} from 'react-native';
+import React from 'react';
+import { View, Button, TouchableOpacity} from 'react-native';
 const StopButton = props => {
   return (
-    <View>
-      <Text> Stop</Text>
+    <View style={{flex: 1, margin:50}}>
       <Button
         title="Stop"
-        onPress={()=> props.player.stop()}></Button>
+        color="#ff0000"
+        onPress={() => props.player.stop()}></Button>
+      <TouchableOpacity
+        activeOpacity={0.95}
+        style={{width: 250}}></TouchableOpacity>
     </View>
   );
 };
